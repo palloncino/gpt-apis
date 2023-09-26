15366caa6a9cfd753ea6b52e560bee707ac3f4ca
palloncino <antonio.guiotto.dev@gmail.com>
2023-09-26 21:30:01 +0200

    style

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const OpenAI = require("openai");
 const express = require('express');
-const axios = require('axios');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -11,11 +10,10 @@ const PORT = 3001; // A port different from your React app
 app.use(bodyParser.json());
 app.use(cors());
 
-const openai = new OpenAI();
+const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
 
 app.post('/api/chat', async (req, res) => {
   const { prompt } = req.body;
-  console.log(1)
     try {
       const completion = await openai.completions.create({
         model: "gpt-3.5-turbo-instruct",
@@ -23,10 +21,8 @@ app.post('/api/chat', async (req, res) => {
         max_tokens: 1500,
         temperature: 0,
       });
-      console.log(1.2)
       res.json({payload: completion.choices[0].text});
     } catch (error) {
-      console.log(2, error)
         res.status(500).json({ error: "Error making request to OpenAI" });
     }
     
